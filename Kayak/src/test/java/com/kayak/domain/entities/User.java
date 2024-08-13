6a746a175b0b9fd9f817114880c29fcfb8abc349
celil.camur <celil.camur@hepsiburada.com>
2024-08-13 21:46:16 +0300

    Last updates.

--- Kayak/src/test/java/com/kayak/domain/entities/User.java
+++
@@ -1,6 +0,0 @@
-package com.kayak.domain.entities;
-
-public class User {
-    public String name;
-    public String job;
-}
--- Kayak/src/test/java/com/kayak/domain/models/response/CreateUserResponse.java
+++
@@ -1,8 +0,0 @@
-package com.kayak.domain.models.response;
-
-public class CreateUserResponse {
-    public String name;
-    public String job;
-    public String id;
-    public String createdAt;
-}
